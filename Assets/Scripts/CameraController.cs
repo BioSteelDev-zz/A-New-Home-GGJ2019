b669dfbfdf3eeb727890095a78da89cbe199228b
BioSteelDev <biosteeldev@gmail.com>
2019-01-26 10:38:07 -0700

    PlayerController
    Initial Movement, no gravity yet.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float lookSmooth = 0.09f;
+    public Vector3 offsetFromTarget = new Vector3(0, 6, -8);
+    public float xTilt = 10;
+
+    Vector3 destination = Vector3.zero;
+    PlayerController charController;
+    float rotateVel = 0;
+
+    void Start()
+    {
+        SetCameraTarget(target);
+    }
+
+    void SetCameraTarget(Transform t)
+    {
+        target = t;
+        
+        if(target != null)
+        {
+            if (target.GetComponent<PlayerController>())
+            {
+                charController = target.GetComponent<PlayerController>();
+            }
+            else
+            {
+                Debug.LogError("The Camera's target needs a character controller.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Camera needs a target.");
+        }
+    }
+
+    // Only occurs after update
+    private void LateUpdate()
+    {
+        //moving
+        MoveToTarget();
+        //rotating
+        LookAtTarget();
+    }
+
+    void MoveToTarget()
+    {
+        destination = charController.TargetRotation * offsetFromTarget;
+        destination += target.position;
+        transform.position = destination;
+    }
+
+    void LookAtTarget()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth); // Lerp but smoother
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float inputDelay = 0.1f;
+    public float forwardVel = 12.0f;
+    public float rotateVel = 100;
+
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput, turnInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    private void Start()
+    {
+        targetRotation = transform.rotation; // Set target rotation to spawn rotation
+        if (GetComponent<Rigidbody>())
+        {
+            rBody = GetComponent<Rigidbody>();
+        }
+        else
+        {
+            Debug.LogError("The character needs a rigidbody.");
+        }
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical"); // Defined in Edit > Project Settings > Input - returns value between -1 and 1
+        turnInput = Input.GetAxis("Horizontal"); // Defined in Edit > Project Settings > Input
+    }
+
+    private void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    private void FixedUpdate()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        // If no longer receiving input, stop moving
+        if(Mathf.Abs(forwardInput) > inputDelay)
+        {
+            // move
+            rBody.velocity = transform.forward * forwardInput * forwardVel;
+        }
+        else
+        {
+            // zero velocity
+            rBody.velocity = Vector3.zero;
+        }
+    }
+    
+    void Turn()
+    {
+        // If stop receiving input, stop turning
+        if(Mathf.Abs(turnInput) > inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up); // Multiplying quaternions is like adding angles to previous target rotation
+            transform.rotation = targetRotation;
+        }
+        transform.rotation = targetRotation;
+    }
+}
