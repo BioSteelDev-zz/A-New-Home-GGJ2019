4a80c02a385febb71a2c1601c8be860b90a121ff
BioSteelDev <biosteeldev@gmail.com>
2019-01-27 12:48:08 -0700

    Added Models
    Title Screen and Splash Screen

--- Assets/Scripts/TitleScreen/PlayButton.cs
+++ Assets/Scripts/TitleScreen/PlayButton.cs
@@ -1,38 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
-    public List<Texture2D> buttonTextures;
-
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(PlayGame);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnMouseOver()
-    {
-        // Change Texture to hover button
-        
-        // Play a sound
-    }
-
-    public void OnMouseExit()
-    {
-        //Change Texture to static button
-    }
-
     public void PlayGame()
     {
-
+        SceneManager.LoadScene(1);
     }
 }
---
+++ Assets/Scripts/TitleScreen/QuitButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(QuitGame);  
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
